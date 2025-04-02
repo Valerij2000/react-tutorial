241883e2bb0a6c9630ffe351e3477fe7914de3b3
Валерий <valery.shumkov@mail.ru>
2025-04-02 23:45:00 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Button from "./components/Button";
 import "./styles/main.scss";
 
 function App() {
-  return <div className="wrapper"></div>;
+  return (
+    <div className="wrapper">
+      <div className="center">
+        <Button />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,34 @@
 export default function Button() {
+  let counter = 0;
+  const handleClick = () => {
+    console.log("Button clicked!");
+  };
+
+  const handleClick2 = (name) => {
+    if (counter === 0) {
+      counter++;
+      alert(`Hello, ${name}!`);
+    } else {
+      alert("You have already clicked the button!");
+    }
+  };
+
+  const handleClick3 = (e) => {
+    e.target.style.backgroundColor = "white";
+    e.target.style.color = "green";
+  };
+
   return (
-    <button className="todo-add-button" id="todo-add-button">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="22"
-        height="22"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="feather feather-plus-circle"
+    <>
+      <button
+        className="btn btn-danger m-4"
+        onClick={() => handleClick2("Valerij")}
       >
-        <circle cx="12" cy="12" r="10"></circle>
-        <line x1="12" y1="8" x2="12" y2="16"></line>
-        <line x1="8" y1="12" x2="16" y2="12"></line>
-      </svg>
-    </button>
+        Click me
+      </button>
+      <button className="btn btn-danger m-4" onClick={(e) => handleClick3(e)}>
+        Click me with event param
+      </button>
+    </>
   );
 }
